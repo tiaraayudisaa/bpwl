36b0503c8c9e1423221492d368130e90593099f8
tiaraayudisaa <65403622+tiaraayudisaa@users.noreply.github.com>
2020-05-15 20:17:01 +0700

    Update index.php

--- chart2/index.php
+++ chart2/index.php
@@ -83,7 +83,7 @@ var theme = {
 // tui.chart.registerTheme('myTheme', theme);
 // options.theme = 'myTheme';
 
-tui.chart.barChart(container, data, options);
+tui.chart.columnChart(container, data, options);
 </script>
 
 <!--For tutorial page-->
